5610b5f5ba9072407a6629d9659fdd232fbb283e
Italo aurelio <italoaurelior@gmail.com>
2021-10-10 20:03:03 -0300

    Configure header

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,1 +1,18 @@
-#include <signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 17:01:25 by itaureli          #+#    #+#             */
+/*   Updated: 2021/10/10 19:50:55 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <sys/types.h>
+# include <signal.h>
+# include "../libs/libft/libft.h"
+#endif
