d393f300f2d3010bec2e5a7572dbdeeec7dccab0
Italo aurelio <italoaurelior@gmail.com>
2021-10-10 20:03:29 -0300

    Testing PID and kill

--- src/client.c
+++ src/client.c
@@ -1,4 +1,32 @@
-int main()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 11:35:06 by itaureli          #+#    #+#             */
+/*   Updated: 2021/10/10 19:47:32 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Params from client:
+-The server PID.
+-The string that should be sent
+- Validate quantity of arguments
+*/
+
+#include "../includes/minitalk.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
 {
+	if (argc < 3)
+		return 0;
+	printf("argv: %s", argv[1]);
+	//  int kill(pid_t pid, int sig );
+	kill(ft_atoi(argv[1]), 10);
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,4 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 11:35:35 by itaureli          #+#    #+#             */
+/*   Updated: 2021/10/10 17:21:06 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Starts and show his PID
+*/
+
+#include "../includes/minitalk.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void handler(int num)
+{
+	write(STDOUT_FILENO, "HI!\n", 4);
+	if(!num) return;
+}
+
 int main()
 {
+	signal(SIGUSR1, handler);
+	while(1)
+	{
+		printf("My PID: %d\n", getpid());
+		sleep(1);
+	}
 	return 0;
 }
