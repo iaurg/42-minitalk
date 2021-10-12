14b4416516105cd353058b336ca1b9f78560c7a9
Italo aurelio <italoaurelior@gmail.com>
2021-10-12 20:05:16 -0300

    Change header position

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,13 +6,13 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 17:01:25 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/10 19:50:55 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/12 18:59:11 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-# include <sys/types.h>
 # include <signal.h>
+# include <sys/types.h>
 # include "../libs/libft/libft.h"
 #endif
