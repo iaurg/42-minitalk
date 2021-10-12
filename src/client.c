16292ddf9f69da2d4a81f8313613c1b31feaa98e
Italo aurelio <italoaurelior@gmail.com>
2021-10-12 20:04:21 -0300

    Add some validations and testing things

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/11 07:35:52 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/12 19:56:16 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,30 @@ Params from client:
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	int pid_i;
+
+	if (argc != 3)
+	{
+		printf("Invalid number of arguments\n");
 		return 0;
-	printf("argv: %s", argv[1]);
+	}
+
+	pid_i = ft_atoi(argv[1]);
+	printf("argv: %s\n", argv[1]);
+	printf("pid_i: %d\n", pid_i);
+
+	// Not allow init process and catcher to all proccess
+	if (pid_i <= 1)
+	{
+		printf("Not allowed PID\n");
+		return 0;
+	}
+	printf("Run Kill\n");
 	//  int kill(pid_t pid, int sig );
-	kill(ft_atoi(argv[1]), SIGUSR1);
+	kill(pid_i, SIGUSR1);
+	/*
+	On kill success  (at least one signal was sent), zero is returned.
+	On error, -1 is returned, and errno is set appropriately.
+	*/
 	return 0;
 }
