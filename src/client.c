fddfc2874ecc6486323a1bfc47ca5765803ebc20
Italo aurelio <italoaurelior@gmail.com>
2021-10-16 16:27:22 -0300

    Validate norminette

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/16 13:23:20 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/16 16:12:44 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,87 +15,71 @@ Params from client:
 -The server PID.
 -The string that should be sent
 - Validate quantity of arguments
+
+[] Validate params are number and string
+[] Allow pass special characters like \n
 */
 
 #include "../includes/minitalk.h"
-#include <stdio.h>
-#include <unistd.h>
 
-char	*binaryString(unsigned short n)
+char	*char_to_bin(unsigned short n)
 {
-	static char bin[8];
-	int x;
-	x = 6;
+	static char	bin[8];
+	int			x;
 
-	while(x >= 0)
+	x = 6;
+	while (x >= 0)
 	{
 		bin[x] = (n % 2) + '0';
 		n = n / 2;
 		x--;
 	}
-
 	bin[7] = '\0';
-	return(bin);
+	return (bin);
 }
 
-static void send_binaries(pid_t processid, int character)
+static void	send_binaries(pid_t process_id, int character)
 {
-	char *binString;
+	char	*bin_string;
 
-	binString = binaryString(character);
-	printf("bin result: %s\n", binString);
-	while (*binString)
+	bin_string = char_to_bin(character);
+	while (*bin_string)
 	{
-		if (*binString == '0')
+		if (*bin_string == '0')
 		{
-			kill(processid, SIGUSR1);
+			kill(process_id, SIGUSR1);
 			usleep(1000);
 		}
-		else if (*binString == '1')
+		else if (*bin_string == '1')
 		{
-			kill(processid, SIGUSR2);
+			kill(process_id, SIGUSR2);
 			usleep(1000);
 		}
-		binString++;
+		bin_string++;
 	}
 }
 
-int main(int argc, char *argv[])
+static void	map_string(char *string_param, pid_t process_id)
 {
-	int pid_i;
-	char *string_param;
-	if (argc != 3)
-	{
-		printf("Invalid number of arguments\n");
-		return 0;
-	}
-	pid_i = ft_atoi(argv[1]);
-	string_param = argv[2];
-	// Not allow init process and catcher to all proccess
-	if (pid_i <= 1)
-	{
-		printf("Not allowed PID\n");
-		return 0;
-	}
-
 	while (*string_param)
 	{
-		printf("c: %c", *string_param);
-		send_binaries(pid_i, (*string_param - 0));
+		send_binaries(process_id, (*string_param - 0));
 		string_param++;
 	}
-	return 0;
 }
 
-/*
-printf("binaryString: %s\n", binaryString(1));
-printf("binaryString: %s\n", binaryString(42));
-printf("binaryString: %s\n", binaryString(127));
+int	main(int argc, char *argv[])
+{
+	int		process_id;
+	char	*string_param;
 
-int kill(pid_t pid, int sig );
-kill(pid_i, SIGUSR1);
-kill(pid_i, SIGUSR2);
-
-On kill success  (at least one signal was sent), zero is returned.
-On error, -1 is returned, and errno is set appropriately.
-*/
+	process_id = ft_atoi(argv[1]);
+	if (argc != 3 || process_id <= 1)
+	{
+		ft_printf("Invalid arguments: ./client <process_id> <string>\n");
+		return (0);
+	}
+	string_param = argv[2];
+	map_string(string_param, process_id);
+	return (0);
+}
--- src/server.c
+++ src/server.c
@@ -6,73 +6,55 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:35 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/16 11:38:46 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/16 16:08:19 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Starts and show his PID
-*/
-
 #include "../includes/minitalk.h"
-#include <stdio.h>
-#include <unistd.h>
 
-int counter = 6;
+int	g_counter = 6;
 
-// a = 110000 = 48
-/*
-holder += 1 << 6;
-holder += 0 << 5;
-holder += 0 << 4;
-holder += 0 << 3;
-holder += 0 << 2;
-holder += 0 << 1;
-holder += 1 << 0;
-*/
-static void bit_to_char(int bit)
+static void	bit_to_char(int bit)
 {
-	static int holder = 0;
-	holder += bit << counter;
-	counter--;
-	if (counter < 0)
+	static int	holder = 0;
+
+	holder += bit << g_counter;
+	g_counter--;
+	if (g_counter < 0)
 	{
 		write(STDOUT_FILENO, &holder, 2);
 		holder = 0;
-		counter = 6;
-		return;
+		g_counter = 6;
+		return ;
 	}
-	return;
+	return ;
 }
 
-static void handler_zero(int sig)
+static void	handler_zero(int sig)
 {
+	if (!sig)
+		return ;
 	bit_to_char(0);
-	if(!sig)
-		return;
 }
 
-static void handler_one(int sig)
+static void	handler_one(int sig)
 {
+	if (!sig)
+		return ;
 	bit_to_char(1);
-	if(!sig)
-		return;
 }
 
-int main()
+int	main(void)
 {
 	struct sigaction	action_zero;
 	struct sigaction	action_one;
 
-	printf("Server PID: %d\n", getpid());
+	ft_printf("Server PID: %d\n", getpid());
 	action_zero.sa_handler = &handler_zero;
 	action_one.sa_handler = &handler_one;
-
 	sigaction(SIGUSR1, &action_zero, NULL);
 	sigaction(SIGUSR2, &action_one, NULL);
 	while (1)
-	{
 		pause();
-	}
-	return 0;
+	return (0);
 }
