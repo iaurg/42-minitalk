4113dc14c70340ad8ca8341bea9fb244614d51e1
Italo aurelio <italoaurelior@gmail.com>
2021-10-16 13:26:16 -0300

    Success print passing via binary

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/15 06:27:55 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/16 13:23:20 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,42 @@ char	*binaryString(unsigned short n)
 	}
 
 	bin[7] = '\0';
-
 	return(bin);
+}
+
+static void send_binaries(pid_t processid, int character)
+{
+	char *binString;
+
+	binString = binaryString(character);
+	printf("bin result: %s\n", binString);
+	while (*binString)
+	{
+		if (*binString == '0')
+		{
+			kill(processid, SIGUSR1);
+			usleep(1000);
+		}
+		else if (*binString == '1')
+		{
+			kill(processid, SIGUSR2);
+			usleep(1000);
+		}
+		binString++;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	int pid_i;
-	char *binString;
-
+	char *string_param;
 	if (argc != 3)
 	{
 		printf("Invalid number of arguments\n");
 		return 0;
 	}
-
 	pid_i = ft_atoi(argv[1]);
-	printf("argv: %s\n", argv[1]);
-	printf("pid_i: %d\n", pid_i);
-
+	string_param = argv[2];
 	// Not allow init process and catcher to all proccess
 	if (pid_i <= 1)
 	{
@@ -61,31 +78,24 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	binString = binaryString(42);
-	while(*binString)
+	while (*string_param)
 	{
-		if (*binString == '1')
-		{
-			printf("1");
-			kill(pid_i, SIGUSR1);
-		}
-		else
-		{
-			printf("0");
-			kill(pid_i, SIGUSR2);
-		}
-		binString++;
+		printf("c: %c", *string_param);
+		send_binaries(pid_i, (*string_param - 0));
+		string_param++;
 	}
-	printf("binaryString: %s\n", binaryString(1));
-	printf("binaryString: %s\n", binaryString(42));
-	printf("binaryString: %s\n", binaryString(127));
-	//  int kill(pid_t pid, int sig );
-	//kill(pid_i, SIGUSR1);
-	//kill(pid_i, SIGUSR2);
-
-	/*
-	On kill success  (at least one signal was sent), zero is returned.
-	On error, -1 is returned, and errno is set appropriately.
-	*/
 	return 0;
 }
+
+/*
+printf("binaryString: %s\n", binaryString(1));
+printf("binaryString: %s\n", binaryString(42));
+printf("binaryString: %s\n", binaryString(127));
+
+int kill(pid_t pid, int sig );
+kill(pid_i, SIGUSR1);
+kill(pid_i, SIGUSR2);
+
+On kill success  (at least one signal was sent), zero is returned.
+On error, -1 is returned, and errno is set appropriately.
+*/
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:35 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/15 06:25:09 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/16 11:38:46 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,61 @@ Starts and show his PID
 #include <stdio.h>
 #include <unistd.h>
 
-void handler(int sig)
+int counter = 6;
+
+// a = 110000 = 48
+/*
+holder += 1 << 6;
+holder += 0 << 5;
+holder += 0 << 4;
+holder += 0 << 3;
+holder += 0 << 2;
+holder += 0 << 1;
+holder += 1 << 0;
+*/
+static void bit_to_char(int bit)
 {
-	write(STDOUT_FILENO, "1\n", 2);
+	static int holder = 0;
+	holder += bit << counter;
+	counter--;
+	if (counter < 0)
+	{
+		write(STDOUT_FILENO, &holder, 2);
+		holder = 0;
+		counter = 6;
+		return;
+	}
+	return;
+}
+
+static void handler_zero(int sig)
+{
+	bit_to_char(0);
 	if(!sig)
 		return;
 }
 
-void handler2(int sig)
+static void handler_one(int sig)
 {
-	write(STDOUT_FILENO, "0\n", 2);
+	bit_to_char(1);
 	if(!sig)
 		return;
 }
 
 int main()
 {
-	struct sigaction	action;
-	struct sigaction	action2;
+	struct sigaction	action_zero;
+	struct sigaction	action_one;
 
-	action.sa_handler = &handler;
-	action2.sa_handler = &handler2;
+	printf("Server PID: %d\n", getpid());
+	action_zero.sa_handler = &handler_zero;
+	action_one.sa_handler = &handler_one;
 
-	sigaction(SIGUSR1, &action, NULL);
-	sigaction(SIGUSR2, &action2, NULL);
-	printf("My PID: %d\n", getpid());
-
-
-	while(1)
+	sigaction(SIGUSR1, &action_zero, NULL);
+	sigaction(SIGUSR2, &action_one, NULL);
+	while (1)
 	{
-		printf("My PID: %d\n", getpid());
-		sleep(1);
+		pause();
 	}
-
 	return 0;
 }
