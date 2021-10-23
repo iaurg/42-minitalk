11dfeecfebac4276300512f7398e88de05db5a35
Italo aurelio <italoaurelior@gmail.com>
2021-10-23 12:52:45 -0300

    Add bonus and better speed

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/23 09:46:38 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/23 12:52:00 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Params from client:
 static void send_signal(int process_id, int signal)
 {
 	kill(process_id, signal);
-	usleep(3000);
+	usleep(9000);
 }
 
 static void char_to_bin(unsigned int n, pid_t process_id)
@@ -52,12 +52,22 @@ static void	send_binaries(pid_t process_id, char *string_param)
 		char_to_bin(*string_param, process_id);
 		string_param++;
 	}
+	char_to_bin(*string_param, process_id);
+}
+
+static void handler_action_client(int sig_number)
+{
+	if(sig_number == SIGUSR2)
+		return;
+	ft_printf("End communication");
+	exit(0);
 }
 
 int	main(int argc, char *argv[])
 {
 	int		process_id;
 	char	*string_param;
+	struct sigaction	action;
 
 	process_id = ft_atoi(argv[1]);
 	if (argc != 3 || process_id <= 1)
@@ -65,6 +75,10 @@ int	main(int argc, char *argv[])
 		ft_printf("Invalid arguments, use: ./client <process_id> <string>\n");
 		return (0);
 	}
+	action.sa_flags = SA_SIGINFO;
+	action.sa_handler = handler_action_client;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
 	string_param = argv[2];
 	send_binaries(process_id, string_param);
 	return (0);
--- src/server.c
+++ src/server.c
@@ -6,13 +6,13 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 11:35:35 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/20 06:59:53 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/23 12:44:24 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-static void	bit_to_char(int bit)
+static void	bit_to_char(int bit, pid_t client_pid)
 {
 	static int	holder = 0;
 	static int	g_counter = 6;
@@ -21,36 +21,35 @@ static void	bit_to_char(int bit)
 	g_counter--;
 	if (g_counter < 0)
 	{
+		if (holder == '\0')
+		{
+			ft_printf("\n");
+			kill(client_pid, SIGUSR1);
+		}
 		ft_printf("%c", holder);
 		holder = 0;
 		g_counter = 6;
 	}
+	kill(client_pid, SIGUSR2);
 }
 
-static void	handler_zero(int sig)
+static void	handler_action(int sig_number, siginfo_t *act_info, void *context)
 {
-	if (!sig)
+	(void)context;
+	if (!sig_number)
 		return ;
-	bit_to_char(0);
-}
-
-static void	handler_one(int sig)
-{
-	if (!sig)
-		return ;
-	bit_to_char(1);
+	bit_to_char(sig_number == SIGUSR2, act_info->si_pid);
 }
 
 int	main(void)
 {
-	struct sigaction	action_zero;
-	struct sigaction	action_one;
+	struct sigaction	action;
 
 	ft_printf("Server PID: %d\n", getpid());
-	action_zero.sa_handler = &handler_zero;
-	action_one.sa_handler = &handler_one;
-	sigaction(SIGUSR1, &action_zero, NULL);
-	sigaction(SIGUSR2, &action_one, NULL);
+	action.sa_flags = SA_SIGINFO;
+	action.sa_sigaction = handler_action;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
 	while (1)
 		pause();
 	return (0);
